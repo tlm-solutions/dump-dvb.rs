469fa31ea43d08fa3d7a96a266e03b7bfb714d3c
revol-xut <revol-xut@protonmail.com>
2022-09-20 18:32:26 +0200

    reduced station for public station list

--- src/management/mod.rs
+++ src/management/mod.rs
@@ -96,6 +96,24 @@ pub struct Station {
     pub antenna: Option<i32>,
 }
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct ReducedStation {
+    pub id: Uuid,
+    pub name: String,
+    pub lat: f64,
+    pub lon: f64,
+    pub region: i64,
+    pub approved: bool,
+    pub deactivated: bool,
+    pub public: bool,
+    pub radio: Option<i32>,
+    pub architecture: Option<i32>,
+    pub device: Option<i32>,
+    pub elevation: Option<f64>,
+    pub telegram_decoder_version: Option<Vec<i32>>,
+    pub antenna: Option<i32>,
+}
+
 #[derive(Debug, Clone, Deserialize, Insertable)]
 #[table_name = "station_history"]
 pub struct StationHistory {
@@ -288,3 +306,24 @@ pub fn user_from_session(connection: &PgConnection, received_token: &String) -> 
     None
 }
 
+
+impl ReducedStation {
+    pub fn from(station: Station) -> ReducedStation {
+        ReducedStation {
+            id: station.id,
+            name: station.name,
+            lat: station.lat,
+            lon: station.lon,
+            approved: station.approved,
+            deactivated: station.deactivated,
+            region: station.region,
+            public: station.public,
+            radio: station.radio,
+            architecture: station.architecture,
+            device: station.device,
+            elevation: station.elevation,
+            telegram_decoder_version: station.telegram_decoder_version.clone(),
+            antenna: station.antenna
+        }
+    }
+}
