1327575b3f03ecef9948157b4d90f28a31ff115e
revol-xut <revol-xut@protonmail.com>
2022-10-02 21:33:22 +0200

    add insertable trekkie run

--- src/trekkie/mod.rs
+++ src/trekkie/mod.rs
@@ -4,15 +4,30 @@ use uuid::Uuid;
 
 use crate::schema::trekkie_runs;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Queryable, Insertable)]
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
 #[diesel(table_name = trekkie_runs)]
 pub struct TrekkieRun {
     pub id: i64,
-    pub start_time: Option<NaiveDateTime>,
-    pub end_time: Option<NaiveDateTime>,
-    pub line: Option<i32>,
-    pub run: Option<i32>,
-    pub gps_file: Option<String>,
+    pub start_time: NaiveDateTime,
+    pub end_time: NaiveDateTime,
+    pub line: i32,
+    pub run: i32,
+    pub gps_file: String,
+    pub region: i64,
+    pub owner: Uuid,
+    pub finished: bool
+}
+
+
+#[derive(Debug, Clone, Serialize, Deserialize, Insertable)]
+#[diesel(table_name = trekkie_runs)]
+pub struct InsertTrekkieRun {
+    pub id: Option<i64>,
+    pub start_time: NaiveDateTime,
+    pub end_time: NaiveDateTime,
+    pub line: i32,
+    pub run: i32,
+    pub gps_file: String,
     pub region: i64,
     pub owner: Uuid,
     pub finished: bool
