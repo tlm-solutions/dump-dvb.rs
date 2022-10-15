5842a31d9e830afef35df786280916dd3c1e494a
Grigory Shipunov <blame@oxapentane.com>
2022-10-15 14:29:21 +0200

    drop non-null values on the way up

--- migrations/2022-10-10-202029_regioback/up.sql
+++ migrations/2022-10-10-202029_regioback/up.sql
@@ -1,5 +1,7 @@
 -- Your SQL goes here
 -- Enterprise fragile migration, history is for suckers and disregarded
 alter table r09_telegrams add column region integer;
-alter table r09_telegrams alter column region set not null;
 update r09_telegrams set region = (select region from stations where r09_telegrams.station = stations.id);
+-- should be empty theoretically, but backups are important!
+delete from r09_telegrams where region is null;
+alter table r09_telegrams alter column region set not null;
