511af313c28209fe015aa4e9178afc02e8ea5d6b
revol-xut <revol-xut@protonmail.com>
2023-03-05 02:28:25 +0100

    adding Insertable to TrekkieRun

--- src/trekkie/mod.rs
+++ src/trekkie/mod.rs
@@ -4,7 +4,7 @@ use uuid::Uuid;
 
 use crate::schema::trekkie_runs;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Queryable)]
+#[derive(Debug, Clone, Serialize, Deserialize, Queryable, Insertable)]
 #[diesel(table_name = trekkie_runs)]
 pub struct TrekkieRun {
     pub id: Uuid,
