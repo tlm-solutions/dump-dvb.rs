99be81df55035ff8645a38983911bd862a8aa281
revol-xut <revol-xut@protonmail.com>
2023-03-05 02:34:19 +0100

    ordering of columns

--- src/trekkie/mod.rs
+++ src/trekkie/mod.rs
@@ -7,7 +7,6 @@ use crate::schema::trekkie_runs;
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable, Insertable)]
 #[diesel(table_name = trekkie_runs)]
 pub struct TrekkieRun {
-    pub id: Uuid,
     pub start_time: NaiveDateTime,
     pub end_time: NaiveDateTime,
     pub line: i32,
@@ -15,5 +14,6 @@ pub struct TrekkieRun {
     pub region: i64,
     pub owner: Uuid,
     pub finished: bool,
+    pub id: Uuid,
 }
 
