b971f0cfc62fd1562600d3502722e630fb436ddb
revol-xut <revol-xut@protonmail.com>
2023-03-05 02:27:02 +0100

    removing InsertTrekkie Run

--- src/trekkie/mod.rs
+++ src/trekkie/mod.rs
@@ -17,15 +17,3 @@ pub struct TrekkieRun {
     pub finished: bool,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize, Insertable)]
-#[diesel(table_name = trekkie_runs)]
-pub struct InsertTrekkieRun {
-    pub id: Option<Uuid>,
-    pub start_time: NaiveDateTime,
-    pub end_time: NaiveDateTime,
-    pub line: i32,
-    pub run: i32,
-    pub region: i64,
-    pub owner: Uuid,
-    pub finished: bool,
-}
