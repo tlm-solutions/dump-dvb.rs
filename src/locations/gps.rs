c57cc5b9d999d3ca04acd18cb4fafb6c2fe8f01e
Grigory Shipunov <blame@oxapentane.com>
2023-03-06 15:57:39 +0100

    move GPS implementations from lofi

--- src/locations/gps.rs
+++ src/locations/gps.rs
@@ -2,9 +2,17 @@ use crate::schema::*;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use gpx::Gpx;
+
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+
+
+pub const NO_ID: i64 = -0xDEADBABE;
 
 // Only parts relevant for the interpolation are here
-/// Gps trackpoint representation used in lofi for Gps data
+/// Gps trackpoint representation used in database for Gps data
 #[derive(Debug, Copy, Clone, Serialize, Deserialize, Queryable)]
 #[diesel(table_name = gps_points)]
 pub struct GpsPoint {
@@ -55,4 +63,82 @@ pub struct InsertGpsPoint {
     pub bearing: Option<f64>,
     /// Optional speed
     pub speed: Option<f64>,
+}
+
+/// Hasmap of gps timestamps to [`GpsPoint`]
+#[derive(Clone, Debug)]
+pub struct Gps(HashMap<i64, GpsPoint>);
+
+
+impl Gps {
+    /// Extracts waypoints from all tracks and segments of a Gpx file
+    pub fn insert_from_gpx_file(&mut self, filepath: &str) {
+        let file = File::open(filepath).expect("Could not open gpx file.");
+        let reader = BufReader::new(file);
+        let gpx = gpx::read(reader).expect("could not parse gpx");
+        Gps::insert_from_gpx(self, gpx)
+    }
+
+    // GPX WayPoint & soul extractor
+    /// Gets Gpx type object and extracts all Waypoints from it, Returns gps::Gps.
+    fn insert_from_gpx(&mut self, gpx: Gpx) {
+        // I feel like my IQ dropping around here, but dunno how to do it, especially given time
+        // situation in gpx crate
+        for track in gpx.tracks {
+            for segment in track.segments {
+                for point in segment.points {
+                    let soul = GpsPoint {
+                        id: NO_ID,
+                        trekkie_run: Uuid::parse_str("00000000-0000-0000-0000-000000000000").unwrap(),
+                        lat: point.point().y(), // according to gpx crate team x and y are less
+                        lon: point.point().x(), // ambiguous for coordinates on a map
+                        elevation: point.elevation,
+                        timestamp: match point.time {
+                            Some(time) => chrono::naive::NaiveDateTime::parse_from_str(
+                                &time.format().unwrap(),
+                                "%Y-%m-%dT%H:%M:%SZ",
+                            )
+                            .unwrap(),
+                            None => break,
+                        },
+
+                        accuracy: point.pdop,
+                        vertical_accuracy: point.vdop,
+                        bearing: None,
+                        speed: point.speed,
+                    };
+
+                    self.insert(soul.timestamp.timestamp(), soul);
+                }
+            }
+        }
+    }
+
+    // hashmap boilerplate
+    /// Exposes hashmap methods on our type alias
+    #[allow(dead_code)]
+    pub fn iter(&self) -> impl Iterator<Item = (&i64, &GpsPoint)> {
+        self.0.iter()
+    }
+
+    /// Exposes hashmap methods on our type alias
+    #[allow(dead_code)]
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = (&i64, &mut GpsPoint)> {
+        self.0.iter_mut()
+    }
+
+    /// Exposes hashmap methods on our type alias
+    pub fn insert(&mut self, k: i64, v: GpsPoint) -> Option<GpsPoint> {
+        self.0.insert(k, v)
+    }
+
+    /// Exposes hashmap methods on our type alias
+    pub fn get(&self, k: &i64) -> Option<&GpsPoint> {
+        self.0.get(k)
+    }
+
+    /// Exposes hashmap methods on our type alias
+    pub fn empty() -> Gps {
+        Gps(HashMap::new())
+    }
 }
