df6eaa24855ea00eb0631f787e7559dcf2b022a1
revol-xut <revol-xut@protonmail.com>
2023-03-08 23:59:51 +0100

    adding docs for lib.rs and trekkie

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,65 @@
+//! 
+//! TLMS.rs is the crate for the Transit Live Mapping Solutions. 
+//!
+//! **Contact:** <hello@tlm.solutions>
+//!
+//! This crate exports a lot of structs, schemas and functionality that is used by the project and 
+//! its services and programs.
+//!
+
 #![deny(missing_docs)]
 #[macro_use]
+
+/// Crate includes so we can access the macros 
 extern crate diesel;
 extern crate dotenv;
 extern crate num_derive;
 
+///
+/// VDV 420 Telegram Definitions
+///
 #[cfg(feature = "telegrams")]
 pub mod telegrams;
 
+///
+/// Rust Diesel Database Schema Definitions
+///
 #[cfg(feature = "schema")]
 pub mod schema;
 
+/// 
+/// This module exports information about locations this includes region informatiom but also 
+/// mappings from reporting point to gps position.
+///
 #[cfg(feature = "locations")]
 pub mod locations;
 
+/// 
+/// Exports the configuration for a Station / Receiver
+///
 #[cfg(feature = "receivers")]
 pub mod receivers;
 
+///
+/// This module / feature exports structs that are required for managing the services like User,
+/// Station, Region and a lot of Enums that are used like AntennaType or Architecture of the
+/// Station.
+///
 #[cfg(feature = "management")]
 pub mod management;
 
+/// 
+/// This module / feature exports structs that are used for measurements that are then later
+/// submitted to trekkie.
+///
 #[cfg(feature = "measurements")]
 pub mod measurements;
 
+///
+/// Trekkie is a service which receives live gps positions, recorded gps measurements 
+/// and correlates them with the r09_telegrams to generate the stops mapping.
+///
+/// This module exports the database structures how trekkie measurements runs are saved in postgres
+///
 #[cfg(feature = "trekkie")]
 pub mod trekkie;
--- src/trekkie/mod.rs
+++ src/trekkie/mod.rs
@@ -4,15 +4,28 @@ use uuid::Uuid;
 
 use crate::schema::trekkie_runs;
 
+/// 
+/// This saves a measurment of a vehicle for later corraltion
+/// This struct tells when the vehicle was measured and which vehicle (line and run)
+/// The gps_table references trekkie_runs for the all  the saves gps locations
+///
 #[derive(Debug, Clone, Serialize, Deserialize, Queryable, Insertable)]
 #[diesel(table_name = trekkie_runs)]
 pub struct TrekkieRun {
+    /// time when the vehicle was entered
     pub start_time: NaiveDateTime,
+    /// time when the vehicle was left
     pub end_time: NaiveDateTime,
+    /// line (ger. linie) of the vehicle
     pub line: i32,
+    /// run (ger. kurs) of the vehicle
     pub run: i32,
+    /// integer representing the integer 
     pub region: i64,
+    /// uuid of the user that made the trekkie run
     pub owner: Uuid,
+    /// flag that tell if the trekkie run is finished or still in progress
     pub finished: bool,
+    /// primary key to uniquely identify a trekkie run
     pub id: Uuid,
 }
