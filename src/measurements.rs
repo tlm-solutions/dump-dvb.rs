a57fb5a146615721b15544d63c68f20b52cb8dda
revol-xut <revol-xut@protonmail.com>
2023-03-09 00:16:36 +0100

    adding docs for measurements

--- src/measurements.rs
+++ src/measurements.rs
@@ -3,12 +3,18 @@ use crate::telegrams::r09::R09SaveTelegram;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
+/// Unfinished measurement of a vehicle used by trekkie or lofi for correlation
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct MeasurementInterval {
+    /// Time the the vehicle was entered
     pub start: Option<NaiveDateTime>,
+    /// Time the vehicle was left
     pub stop: Option<NaiveDateTime>,
+    /// Line (ger. linie) of the tracked vehicle
     pub line: Option<i32>,
+    /// Run (ger. kurs nummer) of the tracked vehicle
     pub run: Option<i32>,
+    /// Integer representing the region in which the vehicle was tracked.
     pub region: Option<i64>,
 }
 
@@ -17,15 +23,20 @@ pub struct MeasurementInterval {
 /// recorded.
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct FinishedMeasurementInterval {
+    /// Time the the vehicle was entered
     pub start: NaiveDateTime,
+    /// Time the vehicle was left
     pub stop: NaiveDateTime,
+    /// Line (ger. linie) of the tracked vehicle
     pub line: i32,
+    /// Run (ger. kurs nummer) of the tracked vehicle
     pub run: i32,
+    /// Integer representing the region in which the vehicle was tracked.
     pub region: i64,
 }
 
 impl FinishedMeasurementInterval {
-    // Converts the intermediate representation into the final measurement
+    /// Converts the intermediate representation into the final measurement
     pub fn from_measurement(measurement: MeasurementInterval) -> FinishedMeasurementInterval {
         FinishedMeasurementInterval {
             start: measurement.start.unwrap(),
@@ -36,7 +47,7 @@ impl FinishedMeasurementInterval {
         }
     }
 
-    // Checks if a given Telegram was recorded in this time interval
+    /// Checks if a given Telegram was recorded in this time interval
     pub fn fits(&self, telegram: &R09SaveTelegram) -> bool {
         if telegram.line.is_none() || telegram.run_number.is_none() {
             return false;
