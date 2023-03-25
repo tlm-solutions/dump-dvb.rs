303b85e973701d29c095da7db2a47ec1844edd26
revol-xut <revol-xut@protonmail.com>
2023-03-25 23:42:25 +0100

    add copy trait to role

--- src/management/user.rs
+++ src/management/user.rs
@@ -19,7 +19,7 @@ use diesel::{
 use std::collections::HashMap;
 
 /// Enum representing the role a user has inside our systems. Values are pretty self-explanatory
-#[derive(Clone, Serialize, Deserialize, PartialEq, Eq, Debug, AsExpression)]
+#[derive(Clone, Copy, Serialize, Deserialize, PartialEq, Eq, Debug, AsExpression)]
 #[diesel(sql_type = diesel::sql_types::Integer)]
 #[allow(missing_docs)]
 pub enum Role {
