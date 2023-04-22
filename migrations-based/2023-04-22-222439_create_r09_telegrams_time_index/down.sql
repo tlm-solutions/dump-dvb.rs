d919c2c01f43a0a402b98ebc7348bfb9977457e9
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2023-04-23 00:32:25 +0200

    drop database query time for grafana dashboards

---
+++ migrations-based/2023-04-22-222439_create_r09_telegrams_time_index/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+
+drop index r09_telegrams_time;
---
+++ migrations-based/2023-04-22-222439_create_r09_telegrams_time_index/up.sql
@@ -0,0 +1,3 @@
+-- Your SQL goes here
+
+create index r09_telegrams_time ON public.r09_telegrams using btree ( time );
